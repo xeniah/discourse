dc682b2a5b3b1964259970176af3d41d41ffcd09
Xenia H <xantippe.h@gmail.com>
2013-02-23 12:28:05 -0800

    Changed secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -7,7 +7,7 @@
 # Generate a new secret with "rake secret".  Copy the output of that command and paste it
 # in your secret_token.rb as the value of Discourse::Application.config.secret_token:
 #
-# Discourse::Application.config.secret_token = "SET_SECRET_HERE" 
+Discourse::Application.config.secret_token = "419a245f95b6396be66b3fc8199dfc3dd9cac2e5d721ff4e812022fb77ff87845e02f31cce6380a35baf8c57b4e576b93df18d8b524de6885b2b29a556a9493a" 
 
 # delete all lines below in production
 # if Rails.env.test? || Rails.env.development? 
