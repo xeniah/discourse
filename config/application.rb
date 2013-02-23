a0f3cdeb470e42c4101f107cfe39638da9d0856c
Xenia H <xantippe.h@gmail.com>
2013-02-23 11:22:38 -0800

    why can;t it find redis.yml? I can find it ...

--- config/application.rb
+++ config/application.rb
@@ -83,6 +83,7 @@ module Discourse
     config.handlebars.templates_root = 'discourse/templates'
 
     # Use redis for our cache
+    #redis_config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
     redis_config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
     # per https://github.com/MichaelCPell/discourse/commit/bedc21e9c9365fc579add3532ae0707f77f0bd5c
     #redis_store = ActiveSupport::Cache::RedisStore.new "redis://#{redis_config['host']}:#{redis_config['port']}/#{redis_config['cache_db']}"
