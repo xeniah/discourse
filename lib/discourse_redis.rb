789be721e54e60452fd3ed5034286f108f50aa03
Xenia H <xantippe.h@gmail.com>
2013-02-23 12:40:11 -0800

    I misspelled a few tings in discourse_redis.rb apparently. Fixed

--- lib/discourse_redis.rb
+++ lib/discourse_redis.rb
@@ -7,7 +7,7 @@ class DiscourseRedis
     @config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
     #redis_opts = {:host => @config['host'], :port => @config['port'], :db => @config['db']} (also line 40)
     # per https://github.com/MichaelCPell/discourse/commit/bedc21e9c9365fc579add3532ae0707f77f0bd5c
-    redis_opts = {:host => @config['host'], :port => @config['port'], :password => @config['password']}' 
+    redis_opts = {:host => @config['host'], :port => @config['port'], :password => @config['password']} 
     @redis = Redis.new(redis_opts)    
   end
 
@@ -21,24 +21,23 @@ class DiscourseRedis
   end
 
   # Proxy key methods through, but prefix the keys with the namespace
-  %w(append blpop brpop brpoplpush decr decrby del exists expire expireat get getbit getrange getset hdel
-       hexists hget hgetall hincrby hincrbyfloat hkeys hlen hmget hmset hset hsetnx hvals incr incrby incrbyfloat
-       lindex linsert llen lpop lpush lpushx lrange lrem lset ltrim mget move mset msetnx persist pexpire pexpireat psetex
-       pttl rename renamenx rpop rpoplpush rpush rpushx sadd scard sdiff set setbit setex setnx setrange sinter
-       sismember smembers sort spop srandmember srem strlen sunion ttl type watch zadd zcard zcount zincrby
-       zrange zrangebyscore zrank zrem zremrangebyrank zremrangebyscore zrevrange zrevrangebyscore zrevrank zrangebyscore).each do |m|
-    define_method m do |*args|
-      args[0] = "#{DiscourseRedis.namespace}:#{args[0]}"
-      @redis.send(__method__, *args)
+    %w(append blpop brpop brpoplpush decr decrby del exists expire expireat get getbit getrange getset hdel
+  hexists hget hgetall hincrby hincrbyfloat hkeys hlen hmget hmset hset hsetnx hvals incr incrby incrbyfloat
+  lindex linsert llen lpop lpush lpushx lrange lrem lset ltrim mget move mset msetnx persist pexpire pexpireat psetex
+  pttl rename renamenx rpop rpoplpush rpush rpushx sadd scard sdiff set setbit setex setnx setrange sinter
+  sismember smembers sort spop srandmember srem strlen sunion ttl type watch zadd zcard zcount zincrby
+  zrange zrangebyscore zrank zrem zremrangebyrank zremrangebyscore zrevrange zrevrangebyscore zrevrank zrangebyscore).each do |m|
+      define_method m do |*args|
+        args[0] = "#{DiscourseRedis.namespace}:#{args[0]}"
+        @redis.send(__method__, *args)
+      end
     end
-  end
 
-  def self.namespace
-    RailsMultisite::ConnectionManagement.current_db
-  end
+    def self.namespace
+      RailsMultisite::ConnectionManagement.current_db
+    end
 
-  def url
-    "redis://redistogo:#{@config['password']}@#{@config['host']}:#{@config['port']}"
-  end
-
+    def url
+      "redis://redistogo:#{@config['password']}@#{@config['host']}:#{@config['port']}"
+    end
 end
