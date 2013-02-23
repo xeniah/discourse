2b7fb36e5fa38f2ff7b4c6523246129839dcd052
Xenia H <xantippe.h@gmail.com>
2013-02-23 12:27:55 -0800

    Changed secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,9 +10,9 @@
 # Discourse::Application.config.secret_token = "SET_SECRET_HERE" 
 
 # delete all lines below in production
-if Rails.env.test? || Rails.env.development? 
-  Discourse::Application.config.secret_token = "47f5390004bf6d25bb97083fb98e7cc133ab450ba814dd19638a78282b4ca291" 
-else 
-  raise "You must set a secret token in config/initializers/secret_token.rb"
-end
+# if Rails.env.test? || Rails.env.development? 
+#   Discourse::Application.config.secret_token = "419a245f95b6396be66b3fc8199dfc3dd9cac2e5d721ff4e812022fb77ff87845e02f31cce6380a35baf8c57b4e576b93df18d8b524de6885b2b29a556a9493a" 
+# else 
+#   raise "You must set a secret token in config/initializers/secret_token.rb"
+# end
 
